4c23ac6787d6bff482be8ac0ee5d3f04d628a50b
Sichkar Yurii <96392712+Yura-Si4kar@users.noreply.github.com>
2022-06-17 23:19:17 +0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const clean = require('gulp-clean');
+var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+function cleanDist() {
+    return gulp.src('dist/*', {read: false})
+        .pipe(clean());
+}
+
+function copyHtml() {
+    return gulp.src('./public/index.html').pipe(
+        gulp.dest('./dist')
+    );
+}
+
+function copyCss() {
+    return gulp.src('src/**/*.css')
+        .pipe(concat('all.css'))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+function copyJs() {
+    return gulp.src('src/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/scripts'));
+}
+
+module.exports = {
+    build: gulp.series(cleanDist, gulp.series(copyHtml,copyCss,copyJs)),
+};
